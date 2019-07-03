41ca8f1062216f35fce5133a676d75775a1ea4f1
Mextrin <seba3552@hotmail.com>
2019-07-03 20:04:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 var rawFile = new XMLHttpRequest();
 rawFile.open("GET", 'ProjectList.txt');
-rawFile.onreadystatechange = function() {
-
-    console.log("functionenter");
-    if (rawFile.readyState === 4 && (rawFile.state === 200 || rawFile.readyState == 0))
-    {   
-        var text = rawFile.responseText;
-        var splitText = text.split('\n');
-        console.log(splitText);
-    }
-};
+var text = rawFile.responseText;
+var splitText = text.split('\n');
+console.log(splitText);
