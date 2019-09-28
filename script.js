fbedd2fdedf5968696c053bdd19f303848a3a8b5
Mextrin <seba3552@hotmail.com>
2019-09-29 01:34:37 +0200

    Code button

---
+++ script.js
@@ -0,0 +1,12 @@
+var currentDisplayID = 'none';
+
+function CodeButton(id) {
+    if (id == currentDisplayID) {
+        document.getElementById(currentDisplayID).style.height = '0px';
+        currentDisplayID = 'none';
+    }
+    else {
+        currentDisplayID = id;
+        document.getElementById(currentDisplayID).style.height = '500px';
+    }
+}
