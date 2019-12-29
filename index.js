92f1d7783fc5aa82305fb295c215d03e4707d9bf
Mextrin <seba3552@hotmail.com>
2019-12-29 07:39:13 +0100

    Minor information changes
    - Initializing pages before render.
    - Changed some descriptions.

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 var currentIndex = 0;
-SetPage(currentIndex);
 
-function SetPage(index)
-{
+function Init() {
+    SetPage(currentIndex, false);
+}
+
+function SetPage(index, animate = true) {
     currentIndex = index;
     var pages = document.getElementsByClassName('contentPage');
     for (i = 0; i < pages.length; i++) {
         pages[i].style.left = window.innerWidth * (i - currentIndex);
         pages[i].style.right = window.innerWidth * -(i - currentIndex);
     }
+}
+
+function SetBackground(index) {
+    
 }
