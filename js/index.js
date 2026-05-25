59fd565513b288140929403e5e95bec714355862
Mextrin <seba3552@hotmail.com>
2026-05-25 05:01:15 +0200

    Animating on click

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 const previewBoxes = document.querySelectorAll(
 	".selectionBox.leftBox, .selectionBox.centerBox, .selectionBox.rightBox"
 );
+const container = document.querySelector(".container");
+const photosBox = document.querySelector(".selectionBox.leftBox");
+const gamesBox = document.querySelector(".selectionBox.centerBox");
+const racingBox = document.querySelector(".selectionBox.rightBox");
 
-const previewDuration = 2500;
+const previewDuration = 5000;
 const nextImageIndexes = new Array(previewBoxes.length).fill(0);
 let previewIndex = 0;
+let previewInterval;
 
 function setPreviewImage(box, imageIndex) {
 	const images = box.querySelectorAll("img");
@@ -40,8 +45,41 @@ function showNextPreview() {
 	previewIndex = (previewIndex + 1) % previewBoxes.length;
 }
 
+function setClickedBox(clickedBox, offsetX, offsetY) {
+	if (previewInterval) {
+		clearInterval(previewInterval);
+		previewInterval = null;
+	}
+
+	container.classList.add("isClicked");
+	container.style.setProperty("--clickedOffsetX", offsetX);
+	container.style.setProperty("--clickedOffsetY", offsetY);
+
+	previewBoxes.forEach(box => {
+		box.classList.toggle("isClickedBox", box === clickedBox);
+	});
+}
+
 if (previewBoxes.length > 0) {
 	previewBoxes.forEach(box => setPreviewImage(box, 0));
 	showNextPreview();
-	setInterval(showNextPreview, previewDuration);
+	previewInterval = setInterval(showNextPreview, previewDuration);
+}
+
+if (container && photosBox) {
+	photosBox.addEventListener("click", () => {
+		setClickedBox(photosBox, "100vw", "0vh");
+	});
+}
+
+if (container && gamesBox) {
+	gamesBox.addEventListener("click", () => {
+		setClickedBox(gamesBox, "0vw", "100vh");
+	});
+}
+
+if (container && racingBox) {
+	racingBox.addEventListener("click", () => {
+		setClickedBox(racingBox, "-100vw", "0vh");
+	});
 }
