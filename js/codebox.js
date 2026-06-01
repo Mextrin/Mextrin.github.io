63ead044e7ae935c49618d4a95981f84d197b8c3
Mextrin <seba3552@hotmail.com>
2026-06-01 05:36:29 +0200

    Revert "Brand new portfolio"
    This reverts commit 4193a5c0bf7cab0360cac73c0983634da72a9c9d, reversing
    changes made to f75f298df74859a14bd2bd498caebc5c7b564317.

--- js/codebox.js
+++ js/codebox.js
@@ -1,81 +1,15 @@
 var currentDisplayID = 'none';
-var generatedCodeboxID = 0;
 
-function CodeButton(buttonOrId, id) {
-    const hasButton = typeof buttonOrId !== 'string';
-    const button = hasButton ? buttonOrId : null;
-    const targetID = hasButton ? id : buttonOrId;
-    const openHeight = 'var(--codebox-open-height)';
-    const codebox = button ? button.closest('.codebox, #codebox') : document;
-    const displays = Array.from(codebox.querySelectorAll('pre'));
-    const targetDisplay = displays.find(display => display.id === targetID) || document.getElementById(targetID);
-
-    if (!targetDisplay) {
-        return;
-    }
-
-    const isOpen = targetDisplay.classList.contains('open');
-
-    displays.forEach(display => {
-        display.style.maxHeight = '0px';
-        display.classList.remove('open');
-    });
-
-    if (codebox !== document) {
-        Array.from(codebox.querySelectorAll('button')).forEach(codeButton => {
-            codeButton.classList.remove('active');
-        });
-    }
-
-    if (isOpen) {
+function CodeButton(id) {
+    if (id == currentDisplayID) {
+        document.getElementById(currentDisplayID).style.maxHeight = '0px';
         currentDisplayID = 'none';
-        return;
     }
-
-    targetDisplay.style.maxHeight = openHeight;
-    targetDisplay.classList.add('open');
-
-    if (button) {
-        button.classList.add('active');
+    else {
+        if (currentDisplayID != 'none')
+            document.getElementById(currentDisplayID).style.maxHeight = '0px';
+        
+        currentDisplayID = id;
+        document.getElementById(currentDisplayID).style.maxHeight = '50%';
     }
-
-    currentDisplayID = targetID;
 }
-
-function InitializeCodeboxes(root = document) {
-    const codeboxes = Array.from(root.querySelectorAll('.codebox, #codebox'));
-
-    codeboxes.forEach(codebox => {
-        const buttons = Array.from(codebox.querySelectorAll('button'));
-
-        if (buttons.length > 0 || codebox.dataset.codeboxInitialized === 'true') {
-            return;
-        }
-
-        const codeBlocks = Array.from(codebox.querySelectorAll('pre'));
-        const tabs = document.createElement('div');
-        tabs.className = 'codeboxTabs';
-
-        codeBlocks.forEach((codeBlock, index) => {
-            const label = codeBlock.dataset.label || codeBlock.id || `Snippet ${index + 1}`;
-
-            if (!codeBlock.id) {
-                generatedCodeboxID++;
-                codeBlock.id = `generatedCodebox${generatedCodeboxID}`;
-            }
-
-            const button = document.createElement('button');
-            button.type = 'button';
-            button.textContent = label;
-            button.addEventListener('click', () => CodeButton(button, codeBlock.id));
-            tabs.appendChild(button);
-        });
-
-        codebox.prepend(tabs);
-        codebox.dataset.codeboxInitialized = 'true';
-    });
-}
-
-window.InitializeCodeboxes = InitializeCodeboxes;
-
-document.addEventListener('DOMContentLoaded', () => InitializeCodeboxes());
--- js/index.js
+++ js/index.js
@@ -1,107 +1,30 @@
-const previewBoxes = document.querySelectorAll(
-	".selectionBox.leftBox, .selectionBox.centerBox, .selectionBox.rightBox"
-);
-const container = document.querySelector(".container");
-const photosBox = document.querySelector(".selectionBox.leftBox");
-const gamesBox = document.querySelector(".selectionBox.centerBox");
-const racingBox = document.querySelector(".selectionBox.rightBox");
+let innerTint = 25;
+let outerTint = 57;
 
-const previewDuration = 5000;
-const indexBackAnimationStepDelay = 250;
-const nextImageIndexes = new Array(previewBoxes.length).fill(0);
-let previewIndex = 0;
-let previewInterval;
+//Resizable buttons
+var buttons = document.getElementsByClassName("resizableButton");
 
-function setPreviewImage(box, imageIndex) {
-	const images = box.querySelectorAll("img");
+for (var i = 0; i < buttons.length; i++) {
+    var videoElement = buttons[i].querySelector(".interactiveVideo");
+        videoElement.pause();
 
-	images.forEach((image, index) => {
-		image.classList.toggle("isPreviewImage", index === imageIndex);
-	});
-}
+    buttons[i].addEventListener("mouseover", function() {
+        var videoElement = this.querySelector(".interactiveVideo");
+        videoElement.play();
 
-function setDynamicTintEnabled(activeBox = null) {
-	previewBoxes.forEach(box => {
-		box.classList.toggle(
-			"dynamicTintEnabled",
-			box === activeBox && box.classList.contains("dynamicTint")
-		);
-	});
-}
+        this.style.width = '75%';
 
-function showNextPreview() {
-	if ([...previewBoxes].some(box => box.matches(":hover"))) {
-		return;
-	}
+        this.style.setProperty('--innerTint', innerTint + '%');
+        this.style.setProperty('--outerTint', outerTint + '%');
+    });
 
-	const fadingOutIndex = (previewIndex + 1) % previewBoxes.length;
-	const waitingIndex = (previewIndex + 2) % previewBoxes.length;
+    buttons[i].addEventListener("mouseleave", function() {
+        var videoElement = this.querySelector(".interactiveVideo");
+        videoElement.pause();
+        videoElement.currentTime = 0;
 
-	previewBoxes.forEach((box, index) => {
-		const isActiveBox = index === previewIndex;
-		const isWaitingBox = index === waitingIndex;
-
-		box.classList.toggle("isPreviewing", isActiveBox || isWaitingBox);
-
-		if (index === fadingOutIndex) {
-			box.classList.remove("isPreviewing");
-			return;
-		}
-
-		if (!isActiveBox) {
-			return;
-		}
-
-		const images = box.querySelectorAll("img");
-
-		if (images.length === 0) {
-			return;
-		}
-
-		setPreviewImage(box, nextImageIndexes[index]);
-		nextImageIndexes[index] = (nextImageIndexes[index] + 1) % images.length;
-	});
-
-	previewIndex = (previewIndex + 1) % previewBoxes.length;
-}
-
-function setClickedBox(clickedBox, offsetX, offsetY) {
-	if (previewInterval) {
-		clearInterval(previewInterval);
-		previewInterval = null;
-	}
-
-	container.classList.add("isClicked");
-	container.style.setProperty("--clickedOffsetX", offsetX);
-	container.style.setProperty("--clickedOffsetY", offsetY);
-
-	previewBoxes.forEach(box => {
-		box.classList.toggle("isClickedBox", box === clickedBox);
-	});
-
-	setDynamicTintEnabled(clickedBox);
-}
-
-if (previewBoxes.length > 0) {
-	previewBoxes.forEach(box => setPreviewImage(box, 0));
-	showNextPreview();
-	previewInterval = setInterval(showNextPreview, previewDuration);
-}
-
-if (container && photosBox) {
-	photosBox.addEventListener("click", () => {
-		setClickedBox(photosBox, "100vw", "0vh");
-	});
-}
-
-if (container && gamesBox) {
-	gamesBox.addEventListener("click", () => {
-		setClickedBox(gamesBox, "0vw", "100vh");
-	});
-}
-
-if (container && racingBox) {
-	racingBox.addEventListener("click", () => {
-		setClickedBox(racingBox, "-100vw", "0vh");
-	});
+        this.style.width = '50%';
+        this.style.setProperty('--innerTint', '0%');
+        this.style.setProperty('--outerTint', '0%');
+    });
 }
